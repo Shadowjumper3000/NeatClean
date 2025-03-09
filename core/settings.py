6f6d150aeb5926e5e23f101c0efa0b69e1f0ec17
David <96634974+Shadowjumper3000@users.noreply.github.com>
2025-03-09 14:44:32 +0100

    feat(docker): local docker dev testing

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,6 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -33,9 +32,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bm+yo)h0ry9ftft*xe5x07@i8f3acria33zxam9#skaj+k(+bi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DEBUG', False))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 
 # Application definition
